3fc1062a239f0445d2fe8df96413be49b37749a4
iczc <iczcalan@gmail.com>
2021-07-29 22:42:29 +0800

    normalizing playground api port

--- deploy/playground/web/vue.config.js
+++ deploy/playground/web/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://localhost:8081/",
+        target: "http://localhost:5000/",
         changeOrigin: true
       },
       "/billboard": {
